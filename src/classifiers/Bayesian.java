7b3d3160a4751bdd182f4bf4c1dd8503f2f5e973
Lava Tahir <lavatahir@gmail.com>
2017-04-01 21:54:45 -0400

    bayesian extending classifier

--- src/classifiers/Bayesian.java
+++ src/classifiers/Bayesian.java
@@ -1,5 +1,17 @@
 package classifiers;
 
-public class Bayesian {
+public class Bayesian extends Classifier{
+
+	@Override
+	public void trainData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double findProbability() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
